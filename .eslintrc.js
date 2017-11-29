5f946585330223c992f8b7f11f8d16b30bc64710
wes.lee <lkh5510@gmail.com>
2017-11-29 23:07:53 +0900

    update config of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,7 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "indent": [
-            "error",
-						2
-        ],
+        "indent": 0,
         "linebreak-style": [
             "error",
             "windows"
