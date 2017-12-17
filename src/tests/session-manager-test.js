88cd849edff73a67bcb98a448377c6a74486512a
이기현 <lkh5510@gmail.com>
2017-12-17 07:18:32 +0000

    resolving #2

--- src/tests/session-manager-test.js
+++ src/tests/session-manager-test.js
@@ -24,4 +24,28 @@ describe('SessionManager', function() {
 			assert(typeof session.id === 'number');
     });
   });
+	
+	describe('#setPartnerById(id)', function() {
+    it('should set partner system when don\'t exists parter without error', function() {
+			var sessionManager = new SessionManager();
+			var session = sessionManager.addSession(null, null);
+			assert(sessionManager.setPartnerById(session.id) === false);
+			assert(sessionManager.getPartnerIdById(session.id) === SessionManager.SERVERID);
+    });
+		it('should return session object by id without error', function() {
+			var sessionManager = new SessionManager();
+			var session = sessionManager.addSession(null, null);
+			assert.deepEqual(sessionManager.getSessionById(session.id), session);
+    });
+		it('should set partner system id when exists some parter without error', function() {
+			var sessionManager = new SessionManager();
+			var session1 = sessionManager.addSession(null, null), session2 = sessionManager.addSession(null, null);
+			
+			assert(sessionManager.setPartnerById(session1.id) === false);
+			assert(sessionManager.setPartnerById(session2.id) === true);
+			
+			assert(sessionManager.getPartnerIdById(session1.id) === session2.id);
+			assert(sessionManager.getPartnerIdById(session2.id) === session1.id);
+    });
+  });
 });
